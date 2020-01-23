848d96fba2afefdd88f82ca9a0b49e83124b5fcb
Simon Chow <Melanmare@users.noreply.github.com>
2020-01-23 19:22:35 +1100

    finish footer

---
+++ src/SCHousekeeper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./component/Footer.js";
+
+function SCHousekeeper() {
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
+}
+
+export default SCHousekeeper;
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "../styles/FooterStyle.scss";
 
+const currentYear = () => {
+  return new Date().getFullYear();
+};
+
+const contactLink = () => {
+  return (
+    <a href="https://simonchow.dev" target="_blank" rel="noopener noreferrer">
+      simonchow.dev
+    </a>
+  );
+};
+
+// footer has a copyright text and a contact link
 const Footer = () => {
   return (
-    <div>
-      <p>this is the footer</p>
+    <div className="footer">
+      <div className="footer-copyright">
+        @{currentYear()} copywrite-simon chow. All rights reserved
+      </div>
+      <div className="footer-contact">Contact me: visit {contactLink()}</div>
     </div>
   );
 };
